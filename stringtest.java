72eaed9b09df8069602ac0c059649b2d6bf4795e
GitHub-sumitra <Suma_chida@yahoo.com>
2014-01-21 20:47:20 +0000

    string test

---
+++ stringtest.java
@@ -0,0 +1,16 @@
+
+public class stringtest {
+	public static void main(String arg[])
+	{
+	  int n=4;
+	  int m=1;
+	  int f=n;
+	  do{f=f*m;
+	  m=m+1;
+	  }while(m!=n);
+	  if(m==n)
+		  System.out.println(f);
+		
+	}
+
+}
